4efd15e1c006dc48283939d17fe3f5687a5ad95e
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-02-10 11:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+# Default to port 5000 if PORT is not set in the environment
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
